af5cbf9f4a0cd187019dfce50b04066827eb6337
Arjun <snarjun92@gmail.com>
2018-02-18 22:08:38 +0530

    auth files

---
+++ src/auth/auth.actiontype.js
@@ -0,0 +1,5 @@
+import { createActionSet } from '../utils';
+
+export const LOGIN = createActionSet('LOGIN');
+export const LOGOUT = createActionSet('LOGOUT');
+export const FETCH_AUTH_USER = createActionSet('FETCH_AUTH_USER');
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
 export * from './method-helper';
+export * from './actions.helper.js';
