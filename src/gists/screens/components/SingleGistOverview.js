1cb84b116834f6c2a61e19404c4596826b21a65c
Arjun <snarjun92@gmail.com>
2018-04-24 21:41:09 +0530

    add more styles

--- src/gists/screens/components/SingleGistOverview.js
+++ src/gists/screens/components/SingleGistOverview.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import sytled from 'styled-components';
+import styled from 'styled-components';
+
+const Container = styled.View`
+	flex-direction: column;
+	padding: 5px 10px;
+`;
+
+const Title = styled.Text`
+	font-weight: bold;
+	margin: 3px 0;
+`
 
 const GistOverview = ({ gistData }) => {
 	return (
-		<View>
-			<Text>{gistData.description}</Text>
+		<Container>
+			<Title>{gistData.description}</Title>
 			<Text>{gistData.created_at}</Text>
-		</View>
+		</Container>
 	)
 }
 
