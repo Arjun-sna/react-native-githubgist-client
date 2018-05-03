5ffdbaec29cae8d2f483abeb730c68cf5bc9a259
prasanna1211 <prasanna@codebrahma.com>
2018-05-03 17:29:55 +0530

    Added uniq list for floatlist and added performance updates

--- src/gists/screens/components/GistContent.js
+++ src/gists/screens/components/GistContent.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { ActivityIndicator, FlatList, View } from 'react-native';
+import {
+	ActivityIndicator,
+	FlatList,
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
+
 import PropTypes from 'prop-types';
+import concat from 'lodash/concat';
+import uniqBy from 'lodash/uniqBy';
 import styled from 'styled-components';
 import GistItem from './SingleGistOverview';
 import EmptyList from './EmptyListComponent';
@@ -13,7 +22,6 @@ const Container = styled.View`
 `;
 
 class GistListContent extends React.Component {
-
 	componentDidMount() {
 		if (this.props.gistList.length < 1) {
 			this.props.fetchGists();
@@ -24,35 +32,60 @@ class GistListContent extends React.Component {
 		this.props.fetchGists();
 	}
 
-	renderListItem = ({ item }) => (
-		<GistItem
-			gistData={item}
-			onClickGist={id => {
-				console.log('clicked ', id);
-			}}
-		/>
-	)
+	renderListItem = ({ item }) => {
+		switch (item.type) {
+		case 'preloader':
+			return (
+				<View style={styles.endOfViewStyle}>
+					<ActivityIndicator
+						size="large"
+					/>
+				</View>
+			);
+		case 'noData':
+			return (
+				<View style={styles.endOfViewStyle}>
+					<Text style={styles.noMoreGistText}>
+						No More Gists exists.
+					</Text>
+				</View>
+			);
+		default:
+			return (
+				<GistItem
+					gistData={item}
+					onClickGist={id => {
+						console.log('clicked ', id);
+					}}
+				/>
+			);
+		}
+	}
 
 	render() {
-		const { gistList, showLoader } = this.props;
+		const { gistList, showLoader, hasMoreData } = this.props;
+		const toAppendData = hasMoreData ? {
+			type: 'preloader',
+			id: 'preloader'
+		} : {
+			type: 'noData',
+			id: 'noData',
+		};
+
+		const uniqGists = uniqBy(concat(gistList, toAppendData), ({ id }) => (id));
 
 		return (
 			<View>
-				{
-					showLoader ? (
-						<ActivityIndicator size="small" />
-					) : (
-						<FlatList
-							data={gistList}
-							keyExtractor={item => item.id}
-							renderItem={this.renderListItem}
-							ItemSeparatorComponent={() => <ListItemSeparator />}
-							onEndReachedThreshold={0.01}
-							onEndReached={this.handleListEndReached}
-							ListEmptyComponent={() => <EmptyList message={this.props.emptyListMessage} />}
-						/>
-					)
-				}
+				<FlatList
+					data={uniqGists}
+					keyExtractor={item => item.id}
+					renderItem={this.renderListItem}
+					ItemSeparatorComponent={() => <ListItemSeparator />}
+					onEndReachedThreshold={0.01}
+					onEndReached={this.handleListEndReached}
+					ListEmptyComponent={() => <EmptyList message={this.props.emptyListMessage} />}
+					removeClippedSubviews
+				/>
 			</View>
 		);
 	}
