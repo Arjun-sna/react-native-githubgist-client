fcb9fd108ce346ca4406dd01563f6c9df781b0d3
Arjun <snarjun92@gmail.com>
2018-05-19 19:50:43 +0530

    list files in gists

---
+++ src/gists/screens/components/GistFileItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
+import CardView from 'react-native-cardview'
+import { colors, normalizeFont } from '../../../config';
+
+const Container = styled(CardView)`
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+	margin: 5px;
+`;
+
+const MetaContainer = styled.View`
+	flex: 1;
+	display: flex;
+	padding: 5px;
+	flex-direction: row;
+`;
+
+const FileName = styled.Text`
+	flex: 1;
+	font-weight: bold;
+	padding: 5px;
+	font-size: ${normalizeFont(16)};
+`;
+
+const MetaDetail = styled.Text`
+	flex: 1;
+	font-size: ${normalizeFont(14)};
+	color: ${colors.greyDark}
+`;
+
+export default ({fileName, language, size}) => {
+	return (
+		<Container cardElevation={2}
+		cardMaxElevation={2}
+		cornerRadius={5}>
+			<FileName>{fileName}</FileName>
+			<MetaContainer>
+				<MetaDetail>{language}</MetaDetail>
+				<MetaDetail>{size}</MetaDetail>
+			</MetaContainer>
+		</Container>
+	)
+}
--- src/gists/screens/gistdetails.screen.js
+++ src/gists/screens/gistdetails.screen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import pick from 'lodash/pick';
+import forOwn from 'lodash/forOwn'
 import Header from './components/GistDetailHeader';
+import GistFileItem from './components/GistFileItem';
+import ListEmptyComponent from './components/EmptyListComponent';
 
 const HeaderProps = [
 	'avatal_url',
@@ -9,16 +12,41 @@ const HeaderProps = [
   'created_at',
 ];
 
-export default ({ navigation }) => {
-	const gistData = navigation.getParam('gistData', {});
-	const { owner } = gistData;
+export default class GistDetails extends React.Component {
+	
+	renderItem = ({item}) =>( 
+			<GistFileItem 
+					fileName={item.filename}
+					language={item.language}
+					size={item.size}/>
+		);
 
-	return(
-		<View>
-			<Header 
-				userImage={owner.avatar_url}
-				userName={owner.login}
-				createdAt={gistData.created_At}/>
-		</View>
-	)
+
+	processFiles = (gistFiles) => {
+		const filesList = [];
+		forOwn(gistFiles, (value) => 	filesList.push(value))
+		return filesList;
+	};
+
+	render() {
+		const { navigation } = this.props;
+		const gistData = navigation.getParam('gistData', {});
+		const { owner } = gistData;
+		const gistFiles = this.processFiles(gistData.files);
+
+		return(
+			<View>
+				<Header 
+					userImage={owner.avatar_url}
+					userName={owner.login}
+					createdAt={gistData.created_At}/>
+				<FlatList
+					data={gistFiles}
+					renderItem={this.renderItem}
+					keyExtractor={item => item.filename}
+					ListEmptyComponent={() => <EmptyList message="Gist doesn't have any file" />}					
+				/>
+			</View>
+		)
+	}
 }
