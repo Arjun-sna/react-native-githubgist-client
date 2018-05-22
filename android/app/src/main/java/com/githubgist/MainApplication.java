4c977158bfd41df2c9663b4199bc24c27037879e
Arjun <snarjun92@gmail.com>
2018-05-22 09:58:42 +0530

    create schemas

--- android/app/src/main/java/com/githubgist/MainApplication.java
+++ android/app/src/main/java/com/githubgist/MainApplication.java
@@ -3,6 +3,8 @@ package com.githubgist;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.kishanjvaghela.cardview.RNCardViewPackage;
 import com.psykar.cookiemanager.CookieManagerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
+            new VectorIconsPackage(),
             new RNCardViewPackage(),
             new CookieManagerPackage()
       );
